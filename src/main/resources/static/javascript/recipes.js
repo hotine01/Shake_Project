766670cef14a01a84226c0ee33803d6b40ce7549
John <hotine@yahoo.com>
2019-10-29 14:29:16 +0000

    finished project

--- src/main/resources/static/javascript/recipes.js
+++ src/main/resources/static/javascript/recipes.js
@@ -8,27 +8,75 @@ function getAllRecipes() {
 	request.open("GET", "http://localhost:8080/recipe");
 
 	request.onload = function () {
-		let list = document.getElementById("recipe-list");
-		list.innerHTML = "";
-		let data = JSON.parse(request.response);
-		for (let recipe of data) {
-			console.log(recipe)
-			let listItem = document.createElement("li");
-			listItem.className = "list-group-item";
-			let text = document.createElement("p");
-			text.innerText = recipe.recipe + " " + recipe.ingredients
-			let button = document.createElement("button");
-			button.addEventListener("click", function() {
-				"deleteRecipe(" +recipe.regno +")"
-
-			} 
-
-			listItem.append(text);
-			listItem.append(button);
-			list.appendChild(listItem);
-		}
+		renderToScreen(JSON.parse(request.response));
 	}
 	request.send();
+}
+
+function renderToScreen(data) {
+	let list = document.getElementById("recipe-list");
+	list.innerHTML = "";
+	for (let recipe of data) {
+		// console.log(recipe)
+		let listItem = document.createElement("li");
+		listItem.className = "list-group-item";
+		let text = document.createElement("div");
+
+		let header = document.createElement("h2");
+		header.innerText = recipe.recipe;
+
+		let paragraph = document.createElement("p");
+		paragraph.innerText = recipe.ingredients;
+
+		text.appendChild(header);
+		text.appendChild(paragraph);
+
+		let button = document.createElement("button");
+		button.innerText = "Delete";
+		button.addEventListener("click", function() {
+			deleteRecipe(recipe.regno);
+		});
+		let button1 = document.createElement("button");
+		button1.innerText = "Edit";
+		button1.addEventListener("click", function edit() {
+			editRecipe(listItem , edit, button1, recipe)
+		});
+		listItem.append(text);
+		listItem.append(button);
+		listItem.append(button1);
+		list.appendChild(listItem);
+	}
+}
+
+
+
+/**
+ * Edits the recipe of the given information
+ * @param {HTMLLIElement} listItem 
+ * @param {Function} func 
+ * @param {HTMLButtonElement} button 
+ * @param {Object} info 
+ */
+function editRecipe(listItem, func, button, info) {
+	button.removeEventListener("click", func);
+	
+	let form = document.createElement("form");
+	let textbox = document.createElement("input");
+	textbox.type = "text";
+	let submit = document.createElement("input");
+	submit.type= "submit";
+	
+	form.onsubmit = function() {
+		let body = info
+		body.ingredients = textbox.value;
+		updateRecipe(body);
+		return false;
+	}
+	form.appendChild(textbox);
+	form.appendChild(submit);
+	listItem.appendChild(form);
+	console.log(listItem);
+	console.log(func)
 }
 
 function addRecipe(form) {
@@ -49,31 +97,27 @@ function addRecipe(form) {
 	return false;
 }
 
-function updateRecipe() {
-	let request = new XMLHttpRequest();
-	request.open("PUT", "http://localhost:8080/recipe");
-
+function updateRecipe(body) {
+	console.log(body);
+	 let request = new XMLHttpRequest();
+	 request.open("PUT", "http://localhost:8080/recipe");
+	request.setRequestHeader("Content-Type", "application/json");
 	request.onload = function () {
-		var recipe = document.getElementById('recipe').value;
-		var ingredients = document.getElementById('ingredients').value;
-		var regno = document.getElementById('regno').value;
-		var masterRecipe = new Object();
-		masterRecipe.recipe = recipe;
-		masterRecipe.ingredients = ingredients;
-		masterRecipe.regno = regno;
-		var masterRecipeJSON = JSON.stringify(masterRecipe)
-	}
-	request.send();
+			getAllRecipes();
+		}
+		var body= JSON.stringify(body)
+		request.send(body);
+		return false
 }
 
 
 
 function deleteRecipe(regno) {
-	const Http = new XMLHttpRequest();
+	const http = new XMLHttpRequest();
 	const url = 'http://localhost:8080/recipe/'+regno;
-	Http.open("DELETE", url);
-	Http.onload = function (e) {
+	http.open("DELETE", url);
+	http.onload = function (e) {
 		getAllRecipes();
 	}
-	request.send();
+	http.send();
 }
